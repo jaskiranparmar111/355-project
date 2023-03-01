91e245b20524a137a46ce827bf26d257dad6605c
jaskiranparmar111 <93218909+jaskiranparmar111@users.noreply.github.com>
2023-03-01 00:10:56 -0500

    Add files via upload

---
+++ 355 project/kitty litter.js
@@ -0,0 +1,44 @@
+const open = document.getElementById('open');
+const close = document.getElementById('remove');
+const container = document.querySelector('.container');
+const scrollingElement = (document.scrollingElement || document.body);
+
+
+
+open.addEventListener('click', () => container.classList.add
+  ('show-nav'))
+open.addEventListener('click', () => scrollingElement.scrollTop = scrollingElement.scrollHeight)
+
+
+
+
+
+function removeShowNav(){
+   close.addEventListener('click', () => scrollingElement.scrollTop = 0)
+  close.addEventListener('click', () => container.classList.remove
+  ('show-nav'))
+}
+
+function SignIn(){
+   location.replace("signin.html")
+}
+
+function SignUp(){
+   location.replace("signup.html")
+}
+
+
+function Home(){
+  location.replace("index.html")
+}
+function Catfood(){
+  location.replace("cat food.html")
+}
+function Cattoys(){
+  location.replace("cat toys.html")
+}
+function kittylitter(){
+  location.replace("kitty litter.html")
+}
+
+
